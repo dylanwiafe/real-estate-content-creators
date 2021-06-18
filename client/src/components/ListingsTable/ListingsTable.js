d787b196652f2e9b54c632c10fd22378831c2ab6
Dylan Wiafe <dylanwiafe@gmail.com>
2021-06-17 22:57:29 -0400

    get request + dynamic data on /client side

--- client/src/components/ListingsTable/ListingsTable.js
+++ client/src/components/ListingsTable/ListingsTable.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-// import axios from "axios";
+import axios from "axios";
 // import editIcon from "../../assets/icons"
 // import deleteIcon from "../../assets/icons"
 // import listingModla from "../ListingsModal/ListingsModal"
@@ -11,59 +11,110 @@ import AerialPhotographyRequest from "../../assets/icons/drone-photo-icon.svg";
 import VirtualTourRequest from "../../assets/icons/virtual-tour-icon.svg";
 import DevelopmentRequest from "../../assets/icons/development-icon.svg";
 import dropdownArrow from "../../assets/icons/dropdown-chevron.svg";
-import DeleteListingModal from "../../components/DeleteListingModal";
+import DeleteListingModal from "../../components/DeleteListingModal/DeleteListingModal";
 class ListingsTable extends Component {
   state = {
     listings: [],
-    showListingModal: false,
+    showDeleteModal: false,
     listingToDelete: {},
   };
 
   //create a component did mount function
-  //inside place an axios get request to localhost:8080/listing
-  //in your .then after set the state to listings: response.data
+  //inside place an axios get request to localhost:8001/listing
+  //in your .then after set the state to listings: response.data]
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8001/listing")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ listings: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   //you'll need a handle delete function that takes in a parameter of ID
+  handleDelete = (id) => {
+    console.log(id);
+    const listingToDelete = this.state.listings.find(
+      (listing) => listing.id === id
+    );
+
+    this.setState({ showDeleteModal: true, listingToDelete });
+  };
+
   //inside the handle delete I'm using the find method on the state
   //of listings to do a strict equals comparison on the id of the item I want to delete
   //to one that exists in the json object
   //then I'm setting the state of the delete function to be true
 
   //you'll need a cancel delete function for the x on the modal
-  //inside this function you'll set the state of showing the modal ton false
+  //inside this function you'll set the state of showing the modal to false
+
+  confirmDelete = () => {
+    console.log("confirm delete?");
+    axios
+      .delete("http://localhost:8001/listings" + this.state.listingToDelete.id)
+      .then((response) => {
+        console.log(response);
+        return axios.get("http://localhost:8001/listings");
+      })
+      .then((response) => {
+        console.log(response);
+        this.setState({ showDeleteModal: false, listings: response.data });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  cancelDelete() {
+    console.log("would you like to close this modal?");
+    const abc = this.setState({ showDeleteModal: false });
+  }
 
   render() {
     return (
       <div className="service">
-        <div class="service__container--title">
-          <h1 class="service__heading">Hire a contractor</h1>
+        {this.state.showDeleteModal && (
+          <DeleteListingModal
+          // listingToDelete={}
+          // confirmDelete={}
+          // cancelDelete={}
+          />
+        )}
+        <div className="service__container--title">
+          <h1 className="service__heading">Hire a contractor</h1>
         </div>
         <div className="service__grid">
           <div className="service__card">
-            <p class="card__heading">lorem ipsum</p>
-            <p class="card__copy">lorem ipsum dolor</p>
+            <p className="card__heading">lorem ipsum</p>
+            <p className="card__copy">lorem ipsum dolor</p>
           </div>
 
           <div className="service__card">
-            <p class="card__heading">lorem ipsum</p>
-            <p class="card__copy">lorem ipsum dolor</p>
+            <p className="card__heading">lorem ipsum</p>
+            <p className="card__copy">lorem ipsum dolor</p>
           </div>
           <div className="service__card">
-            <p class="card__heading">lorem ipsum</p>
-            <p class="card__copy">lorem ipsum dolor</p>
+            <p className="card__heading">lorem ipsum</p>
+            <p className="card__copy">lorem ipsum dolor</p>
           </div>
           <div className="service__card--lg">
-            <p class="card__heading">lorem ipsum</p>
-            <p class="card__copy">lorem ipsum dolor</p>
+            <p className="card__heading">lorem ipsum</p>
+            <p className="card__copy">lorem ipsum dolor</p>
           </div>
           <div className="service__card--lg-2">
-            <p class="card__heading">lorem ipsum</p>
-            <p class="card__copy">lorem ipsum dolor</p>
+            <p className="card__heading">lorem ipsum</p>
+            <p className="card__copy">lorem ipsum dolor</p>
           </div>
         </div>
+
         <div className="responsive-table">
-          <div class="responsive-table__header--title">
-            <h1 class="service__heading">Listings</h1>
+          <div className="responsive-table__header--title">
+            <h1 className="service__heading">Listings</h1>
             <button>Filter</button>
             <button>Export</button>
           </div>
@@ -103,42 +154,50 @@ class ListingsTable extends Component {
             </div>
           </div>
           <div className="responsive-table__body">
-            <div className="responsive-table__row">
-              <div className="responsive-table-body__cell">
-                placeholder value
-              </div>
-              <img class="dropdownCta" src={dropdownArrow} alt="menu-cta" />
-              <button>edit</button>
-              <button>delete </button>
+            {this.state.listings.map((listing) => {
+              return (
+                <div className="responsive-table__row">
+                  <div className="responsive-table-body__cell">
+                    {listing.address}
+                  </div>
+                  <img
+                    className="dropdownCta"
+                    src={dropdownArrow}
+                    alt="menu-cta"
+                  />
+                  <button>edit</button>
+                  <button>delete </button>
 
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                placeholder value
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                placeholder value
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                placeholder value
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                placeholder value
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                1 request
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                1 request
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                1 request
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                1 request
-              </div>
-              <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
-                1 request
-              </div>
-            </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.type || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.dateListed || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.style || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.status || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.TwilightPhotographyRequests || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.droneVideoRequests || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.dronePhotoRequest || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.virtualTourRequests || "null"}
+                  </div>
+                  <div className="responsive-table-body__cell responsive-table-body__cell--hidden">
+                    {listing.developmentRequests}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
